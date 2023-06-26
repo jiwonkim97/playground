51b9b6a4e8c82c5d447bb35027356edd0f5bd3fa
jiwonkim97 <jiwon.kim@int2.us>
2023-06-26 16:33:49 +0900

    Pawn.webp -> pawn.webp

--- my_game_hub/components/Chess/Pieces/Pawn/index.tsx
+++ my_game_hub/components/Chess/Pieces/Pawn/index.tsx
@@ -1,6 +1,6 @@
 import BasePiece, { PieceProps } from '@/components/Chess/Pieces/Basic';
-import PawnBlack from '@/public/asset/chess/Pawn-black.webp';
-import RookWhite from '@/public/asset/chess/Pawn-white.webp';
+import PawnBlack from '@/public/asset/chess/pawn-black.webp';
+import RookWhite from '@/public/asset/chess/pawn-white.webp';
 
 const Pawn = ({ color, position }: PieceProps) => {
   return <BasePiece color={color} blackImage={PawnBlack} whiteImage={RookWhite} type='PAWN' position={position} />;
